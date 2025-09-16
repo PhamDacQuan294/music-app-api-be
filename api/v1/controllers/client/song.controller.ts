644edaca08c40bc73ff8b0e95a3397776af7b4b6
quan <quan93909@gmail.com>
2025-09-16 14:28:00 +0700

    lấy chi tiết bài hát và làm tính năng like

--- api/v1/controllers/client/song.controller.ts
+++ api/v1/controllers/client/song.controller.ts
@@ -34,3 +34,56 @@ export const list = async (req: Request, res: Response) => {
     songs: songs
   })
 }
+
+// [GET] /api/v1/songs/detail/:slugSong
+export const detail = async (req: Request, res: Response) => {
+  const slugSong: string = req.params.slugSong;
+
+  const song = await Song.findOne({
+    slug: slugSong,
+    status: "active",
+    deleted: false
+  });
+
+  const singer = await Singer.findOne({
+    _id: song.singerId,
+    deleted: false,
+  }).select("fullName");
+
+  const topic = await Topic.findOne({
+    _id: song.topicId,
+    deleted: false
+  }).select("title");
+
+  res.json({
+    code: 200,
+    song: song,
+    singer: singer,
+    topic: topic
+  })
+}
+
+// [PATCH] /api/v1/songs/like/:typeLike/:idsong
+export const like = async (req: Request, res: Response) => {
+  const idSong: string = req.params.idSong;
+  const typeLike: string = req.params.typeLike;
+
+  const song = await Song.findOne({
+    _id: idSong,
+    status: "active",
+    deleted: false
+  });
+
+  const newLike: number = typeLike == "like" ? song.like + 1 : song.like - 1;
+
+  await Song.updateOne({
+    _id: idSong
+  }, {
+    like: newLike
+  });
+
+  res.json({
+    code: 200,
+    like: newLike
+  });
+};
