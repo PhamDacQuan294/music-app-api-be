e0ee1e983182855df8f3700cb696e6d1d62b488c
quan <quan93909@gmail.com>
2025-09-18 07:38:57 +0700

    Tìm kiếm hiển thị suggest (gợi ý)

--- api/v1/controllers/client/search.controller.ts
+++ api/v1/controllers/client/search.controller.ts
@@ -4,40 +4,70 @@ import Song from "../../models/song.model";
 import Singer from "../../models/singer.model";
 import FavoriteSong from "../../models/favorite-song.model";
 
-// [GET] /api/v1/search/result
+
+// [GET] /api/v1/search/:type
 export const result = async (req: Request, res: Response) => {
-  const keyword: string = `${req.query.keyword}`;
+  try {
+    const keyword: string = `${req.query.keyword || ""}`;
+    const type: string = req.params.type;
 
-  let newSongs = [];
+    let newSongs: any[] = [];
 
-  if (keyword) {
-    const keywordRegex = new RegExp(keyword, "i");
+    if (keyword) {
+      const keywordRegex = new RegExp(keyword, "i");
 
-    // Tạo ra slug không dấu, có thêm dấu - ngăn cách
-    const stringSlug = convertToSlug(keyword);
-    const stringSlugRegex = new RegExp(stringSlug, "i");
-    
-    const songs = await Song.find({
-      $or: [
-        { title: keywordRegex },
-        { slug: stringSlugRegex }
-      ]
-    }).lean();
+      // Tạo ra slug không dấu, có thêm dấu - ngăn cách
+      const stringSlug = convertToSlug(keyword);
+      const stringSlugRegex = new RegExp(stringSlug, "i");
 
-    for (const item of songs) {
-      const infoSinger = await Singer.findOne({
-        _id: item.singerId
-      });
+      const songs = await Song.find({
+        $or: [
+          { title: keywordRegex },
+          { slug: stringSlugRegex }
+        ]
+      }).lean();
 
-      item["infoSinger"] = infoSinger;
+      for (const item of songs) {
+        const infoSinger = await Singer.findOne({
+          _id: item.singerId
+        });
+
+        newSongs.push({
+          id: item._id,
+          title: item.title,
+          avatar: item.avatar,
+          like: item.like,
+          slug: item.slug,
+          infoSinger: {
+            fullName: infoSinger.fullName
+          }
+        });
+      }
     }
 
-    newSongs = songs;
+    switch (type) {
+      case "result":
+        res.json({
+          code: 200,
+          message: "Thành công",
+          songs: newSongs
+        });
+        break;
+      case "suggest":
+        res.json({
+          code: 200,
+          message: "Thành công",
+          songs: newSongs
+        });
+        break;
+      default:
+        break;
+    }
+
+  } catch (error) {
+    return res.status(500).json({
+      code: 500,
+      message: "Lỗi server"
+    });
   }
-
-  res.json({
-    code: 200,
-    songs: newSongs
-  })
-
 }
