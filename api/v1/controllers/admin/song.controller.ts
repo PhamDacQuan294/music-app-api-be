764d37b82a2233aef47437cd5212121df1125cd4
quan <quan93909@gmail.com>
2025-09-19 12:54:09 +0700

    Làm giao diện tạo bài hát ( admin )

--- api/v1/controllers/admin/song.controller.ts
+++ api/v1/controllers/admin/song.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import Song from "../../models/song.model";
+import Topic from "../../models/topic.model";
+import Singer from "../../models/singer.model";
 
 // [GET] /api/v1/admin/songs
 export const index = async (req: Request, res: Response) => {
@@ -7,10 +9,27 @@ export const index = async (req: Request, res: Response) => {
     deleted: false
   });
 
-  console.log(songs);
-  
   res.json({
     code: 200,
     songs: songs
+  })
+}
+
+// [GET] /api/v1/admin/songs/create
+export const create = async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    status: "active",
+    deleted: false
+  }).select("title");
+
+  const singers = await Singer.find({
+    status: "active",
+    deleted: false
+  }).select("fullName");
+
+  res.json({
+    code: 200,
+    topics: topics,
+    singers: singers
   })
 }
