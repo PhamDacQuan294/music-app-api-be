0144cc8e0c58af69afb731b4bf01b3e1e1f1878d
quan <quan93909@gmail.com>
2025-10-15 10:11:28 +0700

    Làm bộ lọc ca sĩ

--- api/v1/controllers/admin/singer.controller.ts
+++ api/v1/controllers/admin/singer.controller.ts
@@ -1,14 +1,24 @@
 import { Request, Response } from "express";
 import Singer from "../../models/singer.model";
+import { filterStatus } from "../../helpers/filterStatus";
 
 // [GET] /api/v1/admin/singers
 export const index = async (req: Request, res: Response) => {
-  const singers = await Singer.find({
+  const statusFilters = filterStatus(req.query);
+
+  let find = {
     deleted: false
-  });
+  };
+
+  if (req.query.status) {
+    find["status"] = req.query.status;
+  }
+
+  const singers = await Singer.find(find);
 
   res.json({
     code: 200,
-    singers: singers
+    singers: singers,
+    filterStatus: statusFilters,
   })
 }
