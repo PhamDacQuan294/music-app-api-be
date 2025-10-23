8124b0e280c5d4f779c2f149d6c7e67b28d65a7e
quan <quan93909@gmail.com>
2025-10-23 11:20:30 +0700

    làm tạo tài khoản admin

--- api/v1/controllers/admin/account.controller.ts
+++ api/v1/controllers/admin/account.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import Account from "../../models/account.model";
 import Role from "../../models/role.model";
+import md5 from "md5";
 
 // [GET] /admin/accounts
 export const index = async (req: Request, res: Response) => {
@@ -26,5 +27,32 @@ export const index = async (req: Request, res: Response) => {
   res.json({
     code: 200,
     accounts: results
+  })
+}
+
+// [POST] /admin/accounts/create
+export const createPost = async (req: Request, res: Response) => {
+  const emailExist = await Account.findOne({
+    email: req.body.email,
+    deleted: false
+  });
+
+  if (emailExist) {
+    res.json({
+      code: 400,
+      message: "Email này đã tồn tại!",
+    });
+    return;
+  } 
+
+  req.body.password = md5(req.body.password);
+  req.body.avatar = req.body.avatar[0];
+
+  const record = new Account(req.body);
+  await record.save();
+
+  res.json({
+    code: 200,
+    message: "Tạo tài khoản admin thành công!"
   })
 }
--- api/v1/routes/admin/account.route.ts
+++ api/v1/routes/admin/account.route.ts
@@ -1,8 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
+import { uploadFields } from "../../middlewares/admin/uploadCloud.middleware";
+
 const router: Router = Router();
 
 import * as controller from "../../controllers/admin/account.controller"
 
+const upload = multer();
+
 router.get("/", controller.index);
+
+router.post(
+  "/create", 
+  upload.fields([
+    { name: 'avatar', maxCount: 1 }, 
+  ]),
+  uploadFields, 
+  controller.createPost
+);
+
 
 export const accountRoutes: Router = router;
